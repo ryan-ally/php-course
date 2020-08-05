11fa4b856ccff2e08aab913cb3441749ab26b9ac
Ryan Hanrahan <ryanhammerdick@gmail.com>
2020-08-05 07:07:17 -0700

    moved name to correct spot

--- index.php
+++ index.php
@@ -5,12 +5,11 @@
   </head>
   <body>
     <?php
-    // Using two forward slashes we create comments in php.
-    // Within the php tag, we can insert values into our HTML.
+     $name = "PHP Store";
+     $credit = 1000;
+
     echo "<h1>Welcome to ".$name."!</h1>";
     echo "<h2>You have $".$credit." in your wallet.</h2>";";
     ?>
-    $name = "PHP Store";
-    $credit = 1000;
   </body>
 </html>
